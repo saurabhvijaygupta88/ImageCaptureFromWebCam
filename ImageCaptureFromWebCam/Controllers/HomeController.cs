6f6e7ae83f109dbd7ca0a4d8090c7083b4457201
Saurabh Vijay Gupta <saurabhgupta@apttus.com>
2020-03-28 13:09:07 +0530

    Added code for image capture

---
+++ ImageCaptureFromWebCam/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageCaptureFromWebCam.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        // GET: Home
+        public ActionResult CaptureImage()
+        {
+            Session["CapturedImage"] = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Capture()
+        {
+            if (Request.InputStream.Length > 0)
+            {
+                using (StreamReader reader = new StreamReader(Request.InputStream))
+                {
+                    string hexString = Server.UrlEncode(reader.ReadToEnd());
+                    string imageName = DateTime.Now.ToString("dd-MM-yy hh-mm-ss");
+                    string imagePath = string.Format("~/Captures/{0}.png", imageName);
+                    System.IO.File.WriteAllBytes(Server.MapPath(imagePath), ConvertHexToBytes(hexString));
+                    Session["CapturedImage"] = VirtualPathUtility.ToAbsolute(imagePath);
+                }
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ContentResult GetCapture()
+        {
+            string url = Session["CapturedImage"].ToString();
+            Session["CapturedImage"] = null;
+            return Content(url);
+        }
+
+        private static byte[] ConvertHexToBytes(string hex)
+        {
+            byte[] bytes = new byte[hex.Length / 2];
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+            return bytes;
+        }
+
+    }
+}
---
+++ ImageCaptureFromWebCam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImageCaptureFromWebCam.MvcApplication" Language="C#" %>
